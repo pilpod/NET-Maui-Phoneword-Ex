40473025ffd12d3a8103f8c1591a4642ab128206
Giacomo Corso <giacomo.corso@gmail.com>
2023-09-20 19:11:01 +0200

    Translate action and Call action done

--- Phoneword/MainPage.xaml.cs
+++ Phoneword/MainPage.xaml.cs
@@ -2,23 +2,41 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        string translatedNumber;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnTranslate(object sender, EventArgs e)
         {
-            count += 5;
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
         }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                // TODO: dial the phone
+            }
+        }
+
     }
 }
